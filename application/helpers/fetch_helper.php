3f64a73e0a2faa820bb0b8e8009b3f5b8039f1e4
Purabungsu <muhammadnorseputra@gmail.com>
2023-06-13 19:48:23 +0800

    add fetch helpers

---
+++ application/helpers/fetch_helper.php
@@ -0,0 +1,20 @@
+<?php 
+
+if (!function_exists('fetchImages')) {
+
+    /**
+     * fetchImages Post by ID Post
+     * 
+     *  @return bool  */
+    function fetchImages($postId)
+    {
+        $url = "https://www.googleapis.com/blogger/v3/blogs/7792147091805313605/posts/{$postId}?key=AIzaSyBMgrvqLoK_LKur-HfdQpGc90Bo2lYXdi8&fetchImages=true&fetchBody=false";
+        $ci = get_instance();
+        $fetch = $ci->curl->simple_get($url);
+        $result = json_decode($fetch);
+        return $result->images[0]->url;
+    }
+}
+
+
+/* End of file fetch_helper.php and path \application\helpers\fetch_helper.php */
