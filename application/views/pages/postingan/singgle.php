405e25ed8ed60691dc46a77b055937b260201c1f
Purabungsu <muhammadnorseputra@gmail.com>
2023-06-23 22:01:06 +0800

    fix singgle page

--- application/views/pages/postingan/singgle.php
+++ application/views/pages/postingan/singgle.php
@@ -1,6 +1,6 @@
 <div class="container">
     <div class="row">
-        <div class="col-md-10 offset-lg-1">
+        <div class="col-md-12">
         <section class="ads">
         <amp-ad
             layout="responsive"
