3dcc7a1aa58fd08511b36752a9e85c3ff8c5a788
Purabungsu <muhammadnorseputra@gmail.com>
2023-06-23 20:36:25 +0800

    change sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ importScripts('https://cdn.ampproject.org/sw/amp-sw.js');
 AMP_SW.init({
     navigationPreload: true,
     assetCachingOptions: [{
-        regexp: /\.(png|jpg|webp|avif|jpeg|js|css)/,
+        regexp: /\.(png|jpg|webp|avif|jpeg|js|css|woff2|woff)/,
         cachingStrategy: 'CACHE_FIRST'
     }]
 });
