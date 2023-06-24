366e752f829638f3a4e769e2db43719d2cf6b348
Muhammad Nor Seputra <43113176+muhammadnorseputra@users.noreply.github.com>
2023-06-24 10:37:48 +0800

    Update metasocial_helper.php

--- application/helpers/metasocial_helper.php
+++ application/helpers/metasocial_helper.php
@@ -1,12 +1,17 @@
 <?php 
 
 
-if (!function_exists('meta_facebook')) {
+if (!function_exists('extractKeyWords')) {
 
-    function meta_facebook()
-    {
-        
-    }
+    function extractKeyWords($string) {
+ mb_internal_encoding('UTF-8');
+ $stopwords = array("dan", "kepada", "dapat", "yang");
+ $string = preg_replace('/[\pP]/u', '', trim(preg_replace('/\s\s+/iu', '', mb_strtolower($string))));
+ $matchWords = array_filter(explode(' ',$string) , function ($item) use ($stopwords) { return !($item == '' || in_array($item, $stopwords) || mb_strlen($item) <= 2 || is_numeric($item));});
+ $wordCountArr = array_count_values($matchWords);
+ arsort($wordCountArr);
+ return implode(',', array_keys(array_slice($wordCountArr, 0, 10)));
+ }
 }
 
 
